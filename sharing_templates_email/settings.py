b31293d4b08c3335744aaa05d90a7907689218f1
Oskorbin <polozyuk.ser99@gmail.com>
2023-01-28 15:22:22 +0200

    Create base template (pause) Create model core

--- sharing_templates_email/settings.py
+++ sharing_templates_email/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'sharing_email.apps.SharingEmailConfig',
+    'sharing_email',
+    'core',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
