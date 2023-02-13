26e61045530e2f9d63394ef9d9ba7b05f8144d4d
Oskorbin <polozyuk.ser99@gmail.com>
2023-02-13 20:37:08 +0200

    get fix change url

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, re_path
 from . import views
 urlpatterns = [
-    path('',  views.featured_product_view, name=''),
-    path('main_page/', views.index, name='main_page'),
+    #path('',  views.redirect_to_url_main_page, name=''),
+    path('main_page/', views.base, name='main_page'),
 
 ]
--- core/views.py
+++ core/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, redirect
 from .models import Base
 
-def index(request):
+def base(request):
     all_test = Base.objects.all()
     context = {
         #'all_destination': all_destination,
         'Base': all_test,
+        'get_last_url':request.path.split("/",-1)[-2]
     }
     return render(request, 'base.html', context=context)
 
-def featured_product_view(request):
+def redirect_to_url_main_page(request):
     return redirect('/main_page/')
