b3e5470fc4da416d7d24cc4975f52c31e8061414
Oskorbin <polozyuk.ser99@gmail.com>
2023-02-14 19:43:02 +0200

    add hyperlinks to menu items get fix bugs

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path, re_path
 from . import views
 urlpatterns = [
-    #path('',  views.redirect_to_url_main_page, name=''),
+    path('',  views.redirect_to_url_main_page, name=''),
+    path('contacts/',  views.redirect_to_url_main_page, name='contacts'),
+    path('about_me/education/',  views.redirect_to_url_main_page, name='about_me_education'),
+    path('about_me/hobbies/',  views.redirect_to_url_main_page, name='about_me_hobbies'),
+    path('about_me/languages/',  views.redirect_to_url_main_page, name='about_me_languages'),
+    path('about_me/skills/',  views.redirect_to_url_main_page, name='about_me_skills'),
+    path('projects/sharing_emails/',  views.redirect_to_url_main_page, name='projects_sharing_emails'),
+    path('projects/sharing_emails/',  views.redirect_to_url_main_page, name='projects_sharing_emails'),
     path('main_page/', views.base, name='main_page'),
 
 ]
--- core/views.py
+++ core/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render, redirect
 from .models import Base
+from django.utils.translation import get_language
 
 def base(request):
     all_test = Base.objects.all()
     context = {
         #'all_destination': all_destination,
         'Base': all_test,
+        #
         'get_last_url':request.path.split("/",-1)[-2]
     }
     return render(request, 'base.html', context=context)
 
 def redirect_to_url_main_page(request):
-    return redirect('/main_page/')
+    return redirect("/"+get_language()+'/main_page/')
